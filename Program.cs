f7712179a544a16b4705cef75e105e6432546b5f
Lorze <lorenz.widmer@gmx.ch>
2013-02-17 18:30:41 +0100

    created a simple capturing test

--- Program.cs
+++ Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using SharpPcap;
+using SharpPcap.WinPcap;
 
 namespace NetDog
 {
@@ -10,6 +13,72 @@ namespace NetDog
     {
         static void Main(string[] args)
         {
+            CaptureDeviceList devices = CaptureDeviceList.Instance;
+
+            if (devices.Count < 1)
+            {
+                Console.WriteLine("No devices were found on this machine");
+                return;
+            }
+
+            Console.WriteLine("Select Device:");
+            Console.WriteLine("");
+
+            // Print out the available network devices
+            Regex regex = new Regex(".*'(.*)'.*");
+            for (int i = 0; i < devices.Count; i++)
+            {
+                Console.WriteLine("{0}: {1}", i, regex.firstMatch(devices[i].Description));
+            }
+
+            int index = -1;
+            while (true)
+            {
+                try
+                {
+                    index = int.Parse(Console.ReadLine());
+                    if (index >= 0 && index < devices.Count)
+                    {
+                        break;
+                    }
+                }
+                catch (Exception)
+                {
+                    continue;
+                }
+            }
+
+            ICaptureDevice device = devices[index];
+
+            device.OnPacketArrival +=
+                new SharpPcap.PacketArrivalEventHandler(device_OnPacketArrival);
+
+            int readTimeoutMilliseconds = 1000;
+            device.Open(DeviceMode.Promiscuous, readTimeoutMilliseconds);
+
+            Console.WriteLine("-- Listening on {0}, hit 'Enter' to stop...",
+                regex.firstMatch(device.Description));
+
+
+            device.StartCapture();
+
+            // Wait for 'Enter' from the user.
+            Console.ReadLine();
+
+            device.StopCapture();
+            device.Close();
+        }
+
+        /// <summary>
+        /// Prints the type and length of each received packet. Getting the Type
+        /// of a package isn`t yet implemented.
+        /// </summary>
+        private static void device_OnPacketArrival(object sender, CaptureEventArgs args)
+        {
+            int len = args.Packet.Data.Length;
+            string type = "";
+
+            Console.WriteLine("{0}: {1}", type, len);
         }
     }
 }
---
+++ Utility.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace NetDog
+{
+    public static class Utility
+    {
+        /// <summary>
+        ///  Returns the first occurrence of the regular expression specified in the 
+        ///  System.Text.RegularExpressions.Regex constructor in the specified input string
+        /// </summary>
+        public static string firstMatch(this Regex regex, string input)
+        {
+            Match match = regex.Match(input);
+            if (match.Success)
+            {
+                return match.Groups[1].Value;
+            }
+            return "";
+        }
+
+        /// <summary>
+        ///  Retrieves a substring from this instance. The substring lies between two search strings.
+        /// </summary>
+        public static string Substring(this string target, string start, string end)
+        {
+            int startPosition = target.IndexOf(start) + start.Length;
+            int endPosition = target.IndexOf(end, startPosition);
+
+            if (endPosition == -1)
+            {
+                return "";
+            }
+
+            return target.Substring(startPosition, endPosition - startPosition);
+        }
+
+        /// <summary>
+        ///  Returns true if the Array is empty.
+        /// </summary>
+        public static bool Empty(this byte[] data)
+        {
+            for (long i = 0; i < data.LongLength; i++)
+            {
+                if (data[i] != 0)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
