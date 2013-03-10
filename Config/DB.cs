82c7306a1531c61fcd09e0311b49ee936a73ea6b
Lorze <lorenz.widmer@gmx.ch>
2013-03-10 17:55:05 +0100

    cleaned usings

--- Config/DB.cs
+++ Config/DB.cs
@@ -1,93 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
-using System.Reflection;
+﻿using System.Reflection;
 
 namespace NetDog.Config
 {
     static class DB
     {
         private static Path _path = new Path(@".\Config\DB.xml");
-        private static SettingFile _file = null;
-        private static SettingFile File
-        {
-            get
-            {
-                if (_file == null)
-                {
-                    _file = new SettingFile(MethodBase.GetCurrentMethod().DeclaringType);
-                    _file.Load(_path);
-                }
-                return _file;
-            }
-        }
+        private static SettingFile _file = new SettingFile(MethodBase.GetCurrentMethod().DeclaringType, _path);
 
         public static void Save()
         {
             _file.Save(_path);
         }
 
-        public static string User
-        {
-            get
-            {
-                return (string)File["User"];
-            }
-            set
-            {
-                File["User"] = value;
-            }
-        }
+        public static string User { get; set; }
 
-        public static string Password
-        {
-            get
-            {
-                return (string)File["Password"];
-            }
-            set
-            {
-                File["Password"] = value;
-            }
-        }
+        public static string Password { get; set; }
 
-        public static Path Database
-        {
-            get
-            {
-                return (Path)File["Database"];
-            }
-            set
-            {
-                File["Database"] = value;
-            }
-        }
+        public static Path Database { get; set; }
 
-        public static string Charset
-        {
-            get
-            {
-                return (string)File["Charset"];
-            }
-            set
-            {
-                File["Charset"] = value;
-            }
-        }
+        public static string Charset { get; set; }
 
-        public static short Dialect
-        {
-            get
-            {
-                return (short)File["Dialect"];
-            }
-            set
-            {
-                File["Dialect"] = value;
-            }
-        }
+        public static short Dialect { get; set; }
     }
 }
--- Config/NetDog.cs
+++ Config/NetDog.cs
@@ -1,46 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
-using System.Reflection;
+﻿using System.Reflection;
 
 namespace NetDog.Config
 {
     static class NetDog
     {
         private static Path _path = new Path(@".\Config\NetDog.xml");
-
-        private static SettingFile _file = null;
-        private static SettingFile File
-        {
-            get
-            {
-                if (_file == null)
-                {
-                    _file = new SettingFile(MethodBase.GetCurrentMethod().DeclaringType);
-                    _file.Load(_path);
-                }
-                return _file;
-            }
-        }
+        private static SettingFile _file = new SettingFile(MethodBase.GetCurrentMethod().DeclaringType, _path);
 
         public static void Save()
         {
             _file.Save(_path);
         }
 
-        public static Path FirebirdClient
-        {
-            get
-            {
-                return (Path)File["FirebirdClient"];
-            }
-            set
-            {
-                File["FirebirdClient"] = value;
-            }
-        }
+        public static Path FirebirdClient { get; set; }
     }
 }
